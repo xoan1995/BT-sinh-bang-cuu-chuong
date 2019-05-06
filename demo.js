90051d9ebcf061ff4f823416e2fd437420b2005f
Xoan Nguyen <hoanglapnhue@.com>
2019-05-06 16:16:29 +0700

    bang cuu chuong

---
+++ demo.js
@@ -0,0 +1,10 @@
+function bangcuuchuong() {
+    document.write("<table border=1 width=1 00%>")
+for(i=1; 1<=9; i++){
+    document.write("<td>" + n + "x" + i + "=" (n*i));
+    document.write("</td>");
+    document.write("</tr>");
+}
+document.write("</tr></table>")
+}
+bangcuuchuong();
